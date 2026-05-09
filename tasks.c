8a17e486da49922bd1deecc098da992507eb8475
Abdul Manan <abdulmananchaudary@gmail.com>
2026-05-10 03:07:40 +0500

    Fixed Headers Update Merged Final Code Task B approved

--- tasks.c
+++ tasks.c
@@ -3,11 +3,10 @@
 int shared_counter = 0;
 
 
-=======
+
 void run_tasks() {
     for (int i = 1; i <= 5; i++) {
         shared_counter += 2;
         printf("Task B -> %d\n", shared_counter);
     }
 }
->>>>>>> feature/task-B
