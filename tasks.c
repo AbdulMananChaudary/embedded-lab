bf136cfe61e8fe80a474456a1498fbc27c607ba3
Abdul Manan <abdulmananchaudary@gmail.com>
2026-05-10 03:22:05 +0500

    Final RTOS Testing Outcome files Merged Resolved

---
+++ tasks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int shared_counter = 0;
+
+
+
+void run_tasks() {
+    for (int i = 1; i <= 5; i++) {
+        shared_counter += 2;
+        printf("Task B -> %d\n", shared_counter);
+    }
+}
