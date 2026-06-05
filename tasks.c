ac9b5ea998327dd076facde2d9e952eca229336c
Abdul Manan <abdulmananchaudary@gmail.com>
2026-06-05 10:45:31 +0500

    Changing limit from 5 to 10

--- tasks.c
+++ tasks.c
@@ -4,8 +4,11 @@ int shared_counter = 0;
 
 
 
+
+
+
 void run_tasks() {
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 10; i++) {
         shared_counter += 2;
         printf("Task B -> %d\n", shared_counter);
     }
